4f76330354738f525d01df720097f8cbf2090d77
develinu <iwlee.dev@gmail.com>
2022-01-01 12:54:35 +0900

    feat: 탭과 탭 애니메이션 추가

--- src/Detail.js
+++ src/Detail.js
@@ -3,6 +3,8 @@ import { useHistory, useParams } from "react-router-dom";
 import './Detail.scss'
 import styled from 'styled-components'
 import { inventoryContext } from './App'
+import { Nav } from 'react-bootstrap'
+import { CSSTransition } from 'react-transition-group'
 
 
 let Box = styled.div`
@@ -23,6 +25,8 @@ const Detail = ({ shoes, setInventory }) => {
   const targetShoes = shoes.find(_shoes => _shoes.id === parseInt(id))
   const [isHide, setIsHide] = useState(false)
   const [inputSearch, setInputSearch] = useState('')
+  const [tabId, setTabId] = useState(0)
+  const [wowTransitionFlag, setWowTransitionFlag] = useState(false)
   
   useEffect(() => {
     let timer = setTimeout(() => {
@@ -39,7 +43,7 @@ const Detail = ({ shoes, setInventory }) => {
         <Title color="black">Detail</Title>
       </Box>
 
-      <input onChange={(e)=>{ setInputSearch(e.target.value) }} />
+      {/* <input onChange={(e)=>{ setInputSearch(e.target.value) }} /> */}
 
       <div className={`my-alert ${isHide ? "hide": ""}`}>
         <p>재고가 얼마 남지 않았습니다</p>
@@ -61,8 +65,35 @@ const Detail = ({ shoes, setInventory }) => {
           <button className="btn btn-danger" onClick={() => { history.goBack() }}>뒤로가기</button>
         </div>
       </div>
+
+      <Nav className="mt-5" variant="tabs" defaultActiveKey="link-0">
+      <Nav.Item>
+        <Nav.Link eventKey="link-0" onClick={() => { setWowTransitionFlag(false); setTabId(0) }}>link-0</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-1" onClick={() => { setWowTransitionFlag(false); setTabId(1) }}>link-1</Nav.Link>
+      </Nav.Item>
+    </Nav>
+
+    <CSSTransition in={wowTransitionFlag} classNames="wow" timeout={500}>
+      <TabContent id={tabId} setWowTransitionFlag={setWowTransitionFlag} />
+    </CSSTransition>
+
     </div>
   )
+}
+
+function TabContent({ id, setWowTransitionFlag }) {
+
+  useEffect(() => {
+    setWowTransitionFlag(true)
+  })
+  const contentMap = {
+    0: <div>0 번째 내용입니다</div>,
+    1: <div>1 번째 내용입니다</div>,
+    2: <div>2 번째 내용입니다</div>
+  }
+  return contentMap[id]
 }
 
 function Inventory({ count }) {
